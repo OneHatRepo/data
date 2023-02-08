a8f383290037a02c26f72e82840a906e66a3e2cd
Scott Spuler <scott@onehat.com>
2023-02-08 12:22:29 -0800

    SecureStore integration now chunks data, bump revision

--- src/Integration/ReactNative/Repository/SecureStore.js
+++ src/Integration/ReactNative/Repository/SecureStore.js
@@ -7,6 +7,8 @@ import OfflineRepository from '@onehat/data/src/Repository/Offline.js';
 import * as SecureStore from 'expo-secure-store'; // see: https://docs.expo.io/versions/latest/sdk/securestore/
 import _ from 'lodash';
 
+const CHUNK_SEPARATOR = 'CHUNK';
+
 /**
  * Repository representing Expo's SecureStore
  * Uses expo-secure-store package
@@ -25,7 +27,29 @@ class SecureStoreRepository extends OfflineRepository {
 				console.log(this.name, 'SecureStore.get', name);
 			}
 
-			const result = await SecureStore.getItemAsync(this._namespace(name));
+
+			// Check if we need to assemble chunks
+			const
+				record = await SecureStore.getItemAsync(this._namespace(name)),
+				regex = new RegExp('^' + CHUNK_SEPARATOR + '([\\d]+)$'),
+				matches = record.match(regex);
+			let result;
+			if (matches && matches[1]) {
+				const
+					totalChunks = matches[1],
+					chunks = [];
+				let n,
+					recName;
+				for (n = 0; n < totalChunks; n++) {
+					recName = this._namespace(name) + CHUNK_SEPARATOR + n;
+					result = await SecureStore.getItemAsync(recName);
+					chunks.push(result);
+				}
+				result = chunks.join('');
+
+			} else {
+				result = await SecureStore.getItemAsync(this._namespace(name));
+			}
 
 			if (this.debugMode) {
 				console.log(this.name, 'SecureStore.get results', name, result);
@@ -57,7 +81,35 @@ class SecureStoreRepository extends OfflineRepository {
 				console.log(this.name, 'SecureStore.set', name, value);
 			}
 
-			const result = await SecureStore.setItemAsync(this._namespace(name), value);
+			const
+				totalSize = new Blob([value]).size,
+				maxSize = 2000;
+			let result;
+			if (totalSize > maxSize) {
+				// value is too big (values cannot be > 2048 bytes https://docs.expo.dev/versions/latest/sdk/securestore/)
+				// so we need to chunk the value
+				const totalChunks = Math.ceil(totalSize / maxSize),
+					chunks = new Array(totalChunks);
+				let i, o, n,
+					chunkValue,
+					recName;
+				for (i = 0, o = 0; i < totalChunks; ++i, o += maxSize) {
+					chunks[i] = value.substr(o, maxSize)
+				}
+
+				// Save a header into the normal value, which stores the number of chunks, and tells the repository to get the chunks next time it's read
+				recName = this._namespace(name);
+				result = await SecureStore.setItemAsync(recName, CHUNK_SEPARATOR + totalChunks);
+
+				// now save the actual chunks
+				for (n = 0; n < chunks.length; n++) {
+					chunkValue = chunks[n];
+					recName = this._namespace(name) + CHUNK_SEPARATOR + n;
+					await SecureStore.setItemAsync(recName, chunkValue);
+				}
+			} else {
+				result = await SecureStore.setItemAsync(this._namespace(name), value);
+			}
 
 			// if (this.debugMode) {
 			// 	console.log(this.name, 'SecureStore.set results', name, result);
@@ -77,7 +129,24 @@ class SecureStoreRepository extends OfflineRepository {
 				console.log(this.name, 'SecureStore.delete', name);
 			}
 
-			const result = await SecureStore.deleteItemAsync(this._namespace(name));
+
+			// Check if we need to delete chunks
+			const
+				record = await SecureStore.getItemAsync(this._namespace(name)),
+				regex = new RegExp('^' + CHUNK_SEPARATOR + '([\\d]+)$'),
+				matches = record.match(regex);
+			let result;
+			if (matches && matches[1]) {
+				const totalChunks = matches[1];
+				let n,
+					recName;
+				for (n = 0; n < totalChunks; n++) {
+					recName = this._namespace(name) + CHUNK_SEPARATOR + n;
+					result = await SecureStore.deleteItemAsync(recName);
+				}
+			} else {
+				result = await SecureStore.deleteItemAsync(this._namespace(name));
+			}
 
 			// if (this.debugMode) {
 			// 	console.log(this.name, 'SecureStore.delete results', name, result);
