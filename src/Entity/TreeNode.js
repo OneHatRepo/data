f79056ee2cab445b2908a8959ca3f3674f0e97f4
Scott Spuler <scott@onehat.com>
2023-05-29 05:24:04 -0700

    TreeNode WIP

--- src/Entity/TreeNode.js
+++ src/Entity/TreeNode.js
@@ -16,6 +16,9 @@ class TreeNode extends Entity {
 		if (!schema.model.parentIdProperty) {
 			throw new Error('parentIdProperty cannot be empty');
 		}
+		if (!schema.model.hasChildrenProperty) {
+			throw new Error('hasChildrenProperty cannot be empty');
+		}
 
 		/**
 		 * @member {boolean} isTreeNode - Whether this Entity is a TreeNode
@@ -45,7 +48,6 @@ class TreeNode extends Entity {
 
 		// UI State
 		this.isVisible = false;
-
 		this.isExpanded = false;
 	}
 
@@ -59,14 +61,13 @@ class TreeNode extends Entity {
 			throw Error('this.getParentIdProperty is no longer valid. TreeNode has been destroyed.');
 		}
 		const parentIdProperty = this.getSchema().model.parentIdProperty;
-		if (!parentIdProperty) {
-			throw new Error('No parentIdProperty found for ' + schema.name);
-		}
 		return this.getProperty(parentIdProperty);
 	}
 
 	/**
 	 * Gets the parentId for this TreeNode.
+	 * It does this by getting the parentId property's submitValue.
+	 * It doesn't look at some value created by client on the TreeNode.
 	 * @return {any} parentId - The parentId
 	 */
 	getParentId = () => {
@@ -77,7 +78,7 @@ class TreeNode extends Entity {
 	}
 
 	/**
-	 * Getter of the parentId for this TreeNode.
+	 * Getter of parentId for this TreeNode.
 	 * @return {any} parentId - The parentId
 	 */
 	get parentId() {
@@ -86,26 +87,59 @@ class TreeNode extends Entity {
 
 	/**
 	 * Getter of hasParent
-	 * Returns true if this.parent is truthy
+	 * Returns true if this is not a root node
 	 * @return {boolean} hasParent
 	 */
 	get hasParent() {
-		return !!this.parent;
+		return !this.isRoot;
 	}
 
+	/**
+	 * Gets the "hasChildren" Property object for this TreeNode.
+	 * This is the Property whose value represents whether this TreeNode has any children.
+	 * @return {Property} parentId Property
+	 */
+	getHasChildrenProperty = () => {
+		if (this.isDestroyed) {
+			throw Error('this.getHasChildrenProperty is no longer valid. TreeNode has been destroyed.');
+		}
+		const hasChildrenProperty = this.getSchema().model.hasChildrenProperty;
+		return this.getProperty(hasChildrenProperty);
+	}
+
+	/**
+	 * Gets the hasChildren value for this TreeNode.
+	 * It does this by getting the hasChildren property's submitValue.
+	 * It doesn't look at some value created by client on the TreeNode.
+	 * @return {any} parentId - The parentId
+	 */
+	getHasChildren = () => {
+		if (this.isDestroyed) {
+			throw Error('this.getHasChildren is no longer valid. TreeNode has been destroyed.');
+		}
+		return this.getHasChildrenProperty().getSubmitValue();
+	}
+
+	/**
+	 * Getter of hasChildren for this TreeNode.
+	 * @return {any} parentId - The parentId
+	 */
+	get hasChildren() {
+		return this.getHasChildren();
+	}
+
+	/**
+	 * Getter of parent TreeNode for this TreeNode.
+	 * @return {TreeNode} parent - The parent TreeNode
+	 */
 	getParent = () => {
 		if (this.isDestroyed) {
 			throw Error('this.getParent is no longer valid. TreeNode has been destroyed.');
 		}
+		if (!this.hasParent) {
+			return null;
+		}
 		return this.parent;
-	}
-
-	/**
-	 * Getter of hasChildren
-	 * @return {boolean} hasParent
-	 */
-	get hasChildren() {
-		return !_.isEmpty(this.children);
 	}
 
 	getChildren = async () => {
@@ -118,6 +152,7 @@ class TreeNode extends Entity {
 		return this.children;
 	}
 
+	//////////////
 	loadChidren = async () => {
 		if (this.isDestroyed) {
 			throw Error('this.loadChidren is no longer valid. TreeNode has been destroyed.');
@@ -125,6 +160,7 @@ class TreeNode extends Entity {
 		this.children = await this.repository.loadChildTreeNodes(this); // populates the children with a reference to this in child.parent
 		this.isChildrenLoaded = true;
 	}
+	//////////////
 
 	getPrevousSibling = async () => {
 		if (this.isDestroyed) {
