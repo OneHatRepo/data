b86e9118d6259a2d1c5886ce17122a5052220c81
Scott Spuler <scott@onehat.com>
2023-06-03 09:59:11 -0700

    Cypress webpack works, enable all specs option, bump revision

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,42 @@
 import { defineConfig } from "cypress";
+import webpackPreprocessor from "@cypress/webpack-preprocessor";
+import Webpack from "webpack";
 
 export default defineConfig({
 	e2e: {
+		experimentalRunAllSpecs: true,
 		chromeWebSecurity: false,
-		setupNodeEvents(on, config) {
-			// bind to the event we care about
-			// on('<event>', (arg1, arg2) => {
-			// 	// plugin stuff here
-			// })
+		setupNodeEvents(on) {
+			const options = webpackPreprocessor.defaultOptions;
+			if (!options.module) {
+				options.module = {
+					rules: [],
+				}
+			}
+			if (!options.module.rules) {
+				options.module.rules = [];
+			}
+			options.module.rules.push({
+				test: /\.(js|jsx|mjs)$/,
+				exclude: [/node_modules\/(?!(@onehat)\/).*/],
+				use: [{
+					loader: 'babel-loader',
+					options: {
+						cacheDirectory: false,
+						presets: [
+							'@babel/preset-env'
+						],
+						plugins: [
+							'@babel/plugin-transform-class-properties',
+							'@babel/plugin-transform-runtime'
+						],
+						sourceType: 'unambiguous',
+					},
+				}],
+
+			});
+
+			on('file:preprocessor', webpackPreprocessor(options));
 		},
 	}
 });
