f405be2c19e6220385ce031bed52ce1fa4a26482
Scott Spuler <scott@onehat.com>
2023-12-22 20:33:42 -0800

    restore XmlReader and XmlWriter, bump revision

--- src/Reader/index.js
+++ src/Reader/index.js
@@ -1,10 +1,10 @@
 /** @module Reader */
 
 import JsonReader from './JsonReader.js';
-// import XmlReader from './XmlReader.js';
+import XmlReader from './XmlReader.js';
 
 const ReaderTypes = {
 	[JsonReader.type]: JsonReader,
-	// [XmlReader.type]: XmlReader,
+	[XmlReader.type]: XmlReader,
 };
 export default ReaderTypes;
